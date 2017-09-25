a446756e1babe609cd6c25bc72dafe56f3e956f8
Peter Tang <petersktang@gmail.com>
2017-09-25 22:42:35 +0800

    update library versions, add GuiceInjector

--- src/main/g8/src/main/scala/$package$/services/SwaggerDocService.scala
+++ src/main/g8/src/main/scala/$package$/services/SwaggerDocService.scala
@@ -1,24 +1,25 @@
 package $package$.services
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+// import akka.stream.ActorMaterializer
 import com.github.swagger.akka._
 import com.github.swagger.akka.model.Info
 import $package$.http.routes.DummyServiceRoute
 import io.swagger.models.ExternalDocs
 import io.swagger.models.auth.{ApiKeyAuthDefinition, In}
 
-import scala.reflect.runtime.{universe => ru}
+// import scala.reflect.runtime.{universe => ru}
 
 
 class SwaggerDocService(address: String, port: Int, system: ActorSystem)
   extends SwaggerHttpService
-    with HasActorSystem {
-  override implicit val actorSystem: ActorSystem = system
-  override implicit val materializer: ActorMaterializer = ActorMaterializer()
-  override val apiTypes = Seq(
-    ru.typeOf[DummyServiceRoute]
-  )
+  // with HasActorSystem {
+  //override implicit val actorSystem: ActorSystem = system
+  //override implicit val materializer: ActorMaterializer = ActorMaterializer()
+  //override val apiTypes = Seq(
+  //  ru.typeOf[DummyServiceRoute]
+  //)
+  override val apiClasses = Set(classOf[DummyServiceRoute])
   override val host = address + ":" + port
   override val info = Info(version = "1.0")
   override val externalDocs = Some(new ExternalDocs("More Info", "#noset"))
